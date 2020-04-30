b7b594825f7328e8545477e0c155f2c2be1900b6
Trent A Miller <46724737+TrentAMiller@users.noreply.github.com>
2020-04-29 21:27:51 -0400

    TapTap

---
+++ TapGameFinal/Assets/GameControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+
+    public Text strokeText;
+    public int strokesAmount;
+    public Text keepGoing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        strokeText.text = strokesAmount + "Clicks";
+
+    }
+
+    public void IncreaseStrokesAmount()
+    {
+        strokesAmount += 1;
+       
+    }
+}
